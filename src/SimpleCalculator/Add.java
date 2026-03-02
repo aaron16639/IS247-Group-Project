38c079623e9ec25d8f7ad719b6b95b8f987f56a0
aaronsaudager <aaron16639@icloud.com>
2026-03-02 18:54:35 -0500

    Actual final code commit

--- src/SimpleCalculator/Add.java
+++ src/SimpleCalculator/Add.java
@@ -5,6 +5,5 @@ public class Add {
     public static int add(int a ,int b){
         System.out.println("Test");
         return a+b;
-
     }
 }
--- src/SimpleCalculator/Square.java
+++ src/SimpleCalculator/Square.java
@@ -1,7 +1,6 @@
 package SimpleCalculator;
 
 public class Square {
-
     public static int square(int a){
         return a*a;
     }
