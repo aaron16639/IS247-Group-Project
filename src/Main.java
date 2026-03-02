a3bf0bad59374719a53afba134a1568b8f80beea
aaronsaudager <aaron16639@icloud.com>
2026-03-02 18:37:01 -0500

    Added commands referencing the SimpleCalculator classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import SimpleCalculator.Add;
+import SimpleCalculator.Multiplication;
+import SimpleCalculator.Square;
+import SimpleCalculator.Subtract;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Add.add(1, 2);
+        Multiplication.multiply(4, 4);
+        Square.square(2);
+        Subtract.subtract(10, 5);
     }
 }
