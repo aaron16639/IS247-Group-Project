089d368a705e0dc3f000742357a613b3302c694c
aaronsaudager <aaron16639@icloud.com>
2026-03-06 09:51:12 -0500

    Moved Group Task 2 code out

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
-import SimpleCalculator.Add;
-import SimpleCalculator.Multiplication;
-import SimpleCalculator.Square;
-import SimpleCalculator.Subtract;
-
 public class Main {
     public static void main(String[] args) {
-        // Values
-        int sum = Add.add(1, 2);
-        int product = Multiplication.multiply(4, 4);
-        int squared = Square.square(2);
-        int difference = Subtract.subtract(10, 5);
 
-        // Printouts
-        System.out.println("Addition: " + sum);
-        System.out.println("Product: " + product);
-        System.out.println("Squared: " + squared);
-        System.out.println("Difference: " + difference);
     }
 }
--- src/SimpleCalculator/Multiplication.java
+++
@@ -1,7 +0,0 @@
-package SimpleCalculator;
-
-public class Multiplication {
-    public static int multiply(int a, int b) {
-        return a * b;
-    }
-}
