81a48b3338cfbc3320e35579f9a3370c6884b10b
jake alokones <jakoba1@umbc.edu>
2026-04-20 18:17:39 -0400

    added a partial invoice class that extends transaction. still need to write overrides for it tho

---
+++ src/Finance/Invoice.java
@@ -0,0 +1,29 @@
+package Finance;
+
+//class to handle invoices and their invoice payments
+
+import java.time.LocalDate;
+
+public class Invoice extends Transaction {
+
+    private String vendor;
+    private LocalDate dueDate;
+    private boolean paid;
+
+    public Invoice(String transactionId, double amount, LocalDate date, String description,
+                   String vendorName, LocalDate dueDate) {
+        super(transactionId, amount, date, description);
+        this.vendor = vendorName;
+        this.dueDate = dueDate;
+        this.paid = false;
+
+    }
+
+    public String getVendor() { return vendor; }
+    public LocalDate getDueDate() { return dueDate; }
+    public boolean isPaid() { return paid; }
+
+    public void markAsPaid(boolean paid) { this.paid = true; }
+
+    //still need to write the overrides for process transaction and get details
+}
