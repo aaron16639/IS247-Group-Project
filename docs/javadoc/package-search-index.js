a3f9c20e694fdd5ef389269dd5f041e8fe9f52cf
aaronsaudager <aaron16639@icloud.com>
2026-05-10 20:00:58 -0400

    Created JavaDocs for Common and HumanResources

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"HumanResources"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"Common"},{"l":"HumanResources"}];updateSearchResults();
