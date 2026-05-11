d2bbd33c3815b41961772912c6ef7fd76417fb43
aaronsaudager <aaron16639@icloud.com>
2026-05-10 22:04:38 -0400

    Added JavaDocs to docs folder

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"Common"},{"l":"CustomerRelationshipManagement"},{"l":"Finance"},{"l":"HumanResources"},{"l":"SupplyChain"}];updateSearchResults();
